250b130f6651bbd5dfd04c3698773d4a1ba80b2a
Chun-Hao Liu <chun-haoliu@chun-haos-mbp.lan>
2019-12-26 22:52:33 -0800

    More kervolution studies on MNIST.

--- plots.py
+++ plots.py
@@ -26,6 +26,10 @@ filename5 = './performance/MNIST_kerv_conv_poly_trainable_statistics.txt'
 with open(filename5, 'rb') as fp:
     test_acc5 = pickle.load(fp)
 
+filename6 = './performance/MNIST_kervolution_poly_trainable_statistics.txt'
+with open(filename6, 'rb') as fp:
+    test_acc6 = pickle.load(fp)
+
 plt.figure(1)
 plt.plot(np.arange(len(test_acc0)),test_acc0,'-',\
          np.arange(len(test_acc1)),test_acc1,'--',\
@@ -33,9 +37,11 @@ plt.plot(np.arange(len(test_acc0)),test_acc0,'-',\
          np.arange(len(test_acc3)),test_acc3,'--',\
          np.arange(len(test_acc4)),test_acc4,'-.',\
          np.arange(len(test_acc5)),test_acc5,':',\
+         np.arange(len(test_acc6)),test_acc6,'--',\
          linewidth=2, markersize=8)
 plt.ylabel('Validation Accuracy')
 plt.xlabel('Training Step')
 plt.grid()
-plt.legend(['convolution','kervolution - polynomial','kervolution - sigmoid','kervolution - gaussian','kerception A','kerv - conv - polynomial'])
+plt.legend(['conv-conv','kerv-kerv: polynomial (fixed)','ker -kerv: sigmoid (fixed)','kerv-kerv: gaussian (fixed)','kerception A-conv (trainable)','kerv-conv: polynomial (trainable)', 'kerv-kerv: polynomial (trainable)'])
+plt.title('MNIST')
 plt.show()
