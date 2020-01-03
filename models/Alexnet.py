31c9e2aa455823f6472a2f1e8f73a6e4a3f171b1
Chun-Hao Liu <chun-hao.liu@ericsson.com>
2020-01-02 17:18:13 -0800

    Add pure Alexnet.

---
+++ models/Alexnet.py
@@ -0,0 +1,68 @@
+import tensorflow as tf
+from tensorflow.keras.models import Sequential
+from tensorflow.keras.layers import Dense, Activation, Dropout, Flatten, Conv2D, MaxPooling2D
+from tensorflow.keras.layers import BatchNormalization
+import numpy as np
+
+def Alexnet(input_shape,output_shape):
+
+    #Instantiate an empty model
+    model = Sequential()
+
+    # 1st Convolutional Layer
+    model.add(Conv2D(filters=96, input_shape=input_shape, kernel_size=(11,11), strides=(4,4), padding='valid'))
+    model.add(Activation('relu'))
+    # Max Pooling
+    model.add(MaxPooling2D(pool_size=(2,2), strides=(2,2), padding='valid'))
+    # Batch Normalisation before passing it to the next layer
+    model.add(BatchNormalization())
+    
+    # 2nd Convolutional Layer
+    model.add(Conv2D(filters=256, kernel_size=(1,1), strides=(1,1), padding='valid'))
+    model.add(Activation('relu'))
+    # Max Pooling
+    model.add(MaxPooling2D(pool_size=(2,2), strides=(2,2), padding='valid'))
+    # Batch Normalisation
+    model.add(BatchNormalization())
+    
+    # 3rd Convolutional Layer
+    model.add(Conv2D(filters=384, kernel_size=(3,3), strides=(1,1), padding='valid'))
+    model.add(Activation('relu'))
+    # Batch Normalisation
+    model.add(BatchNormalization())
+    
+    # 4th Convolutional Layer
+    model.add(Conv2D(filters=384, kernel_size=(3,3), strides=(1,1), padding='valid'))
+    model.add(Activation('relu'))
+    
+    # 5th Convolutional Layer
+    model.add(Conv2D(filters=256, kernel_size=(3,3), strides=(1,1), padding='valid'))
+    model.add(Activation('relu'))
+    # Max Pooling
+    model.add(MaxPooling2D(pool_size=(2,2), strides=(2,2), padding='valid'))
+    # Batch Normalisation
+    model.add(BatchNormalization())
+    
+    # Passing it to a Fully Connected layer
+    model.add(Flatten())
+    # 1st Fully Connected Layer
+    model.add(Dense(4096))
+    model.add(Activation('relu'))
+    # Add Dropout to prevent overfitting
+    model.add(Dropout(0.5))
+    # Batch Normalisation
+    model.add(BatchNormalization())
+    
+    # 2nd Fully Connected Layer
+    model.add(Dense(4096))
+    model.add(Activation('relu'))
+    # Add Dropout
+    model.add(Dropout(0.5))
+    # Batch Normalisation
+    model.add(BatchNormalization())
+    
+    # Output Layer
+    model.add(Dense(output_shape))
+    model.add(Activation('softmax'))
+    
+    return model
